51f2dc32b4ce9cd3f48ef44ed5c99deffa7ddf8a
RajdeepSarkar2000 <rajdeepsarkar999@gmail.com>
2022-07-06 00:03:50 +0530

    Added verification

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req,res,next) => {
     try {
@@ -27,11 +28,17 @@ export const login = async (req,res,next) => {
         if(!user) return next(createError(404,"User not found"))
         
         const isPassword = await bcrypt.compare(req.body.password,user.password);
-        if(!isPassword) return next(createError(400,"Wrong password"))
-        
+        if(!isPassword) 
+            return next(createError(400,"Wrong password"))
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin},process.env.JWT);
+          
 
         const {password, isAdmin, ...otherDetails} = user;
-        res.status(200).json({...otherDetails});
+        res.cookie("Access_token",token, {
+            httpOnly: true
+        })
+        .status(200).json({...otherDetails});
     }catch(err){
         next(err);
     }
